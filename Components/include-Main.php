ce36b6ce312a7caffd83a768a728250bcfd395b0
Jiyoo <42067334+S-o-r-a@users.noreply.github.com>
2022-11-07 07:25:57 +0800

    added some commentss

--- Components/include-Main.php
+++ Components/include-Main.php
@@ -7,7 +7,7 @@
   <title>Include#1</title>
   <link rel="stylesheet" href="css\bootstrap.min.css"/>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="main-css\sidebar.css"/>
+  <link rel="stylesheet" href="main-css\main.css"/>
   <script type="text/javascript" src="js\bootstrap.min.js"></script>
 </head>
 </html>
