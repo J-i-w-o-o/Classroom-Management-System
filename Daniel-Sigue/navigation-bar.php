dd4b3e12110ec27ca506a4f7f26edbd1fc58e2e1
Jiyoo <42067334+S-o-r-a@users.noreply.github.com>
2022-11-08 16:57:08 +0800

    update

--- Daniel-Sigue/navigation-bar.php
+++ Daniel-Sigue/navigation-bar.php
@@ -1,28 +1,7 @@
-<<<<<<< HEAD
-<?php 
-
-    include('C:\xampp\htdocs\Classroom-Management-System\Components\include-Main.php');
-
-?> 
-
-=======
->>>>>>> 14a6bc83fee4602aa6c86b6c1ba0a48fb432ba0d
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-<<<<<<< HEAD
- <!--   <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="navigation-bar.css">
-    <title>navigation bar</title>
-=======
     <?php
     require('../Components/include-Main.php');
     session_start();
@@ -34,37 +13,9 @@
         header("location: /db/");
     }
     ?>
->>>>>>> 14a6bc83fee4602aa6c86b6c1ba0a48fb432ba0d
 </head>
 
 <body>
-<<<<<<< HEAD
-
-
-  <nav class="navbar navbar-dark bg-dark">
-  <div class="container-fluid">
-  <form class="d-flex">
-  <button class="navbar-toggler" type="button" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="#">Classroom</a>
-            </li>
-    </form>
-        <ul class="nav">
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Mark</a>
-          </li>
-        </ul>
-    </div>
-  </nav>
-=======
     <!--wrapper start-->
     <div class="wrapper">
         <!--header menu start-->
@@ -147,7 +98,6 @@
             });
         });
     </script>
->>>>>>> 14a6bc83fee4602aa6c86b6c1ba0a48fb432ba0d
 
 </body>
 
