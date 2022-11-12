27ae26ffd41716c78b367a71739612d991f3ea2c
Jiyoo <42067334+S-o-r-a@users.noreply.github.com>
2022-11-13 00:51:03 +0800

    asdad

--- admin/usermanage.php
+++ admin/usermanage.php
@@ -25,11 +25,6 @@ if (isset($_SESSION['user'])) {
       <input type="submit" name="submit" value="ADD USER">
     </form>
 
-    <!-- NOTE FOR PATWIK : ETO AY PARA SA PAG PALIT NG PROFILE PIC, HINDI PAG ASSIGN NG PROFILE PIC GAMIT ADMIN -->
-    <form method="post" enctype="multipart/form-data">
-      <input type="file" name="file">
-      <button type="submit" name="upload">Upload</button>
-    </form>
   </div>
 </div>
 <?php
