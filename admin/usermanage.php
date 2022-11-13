46f4bde2db757222e74a7001f2ca79c69052a042
dnlsigue <82865898+dnlsigue@users.noreply.github.com>
2022-11-13 12:47:25 +0800

    edit usermanage

--- admin/usermanage.php
+++ admin/usermanage.php
@@ -39,16 +39,12 @@ if (isset($_SESSION['user'])) {
         <input type="text" class="form-control" placeholder="Address" aria-label="SchoolID" aria-describedby="basic-addon1">
         </div>
 
-        <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false">
-          Dropdown
-        </button>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
-          <li><button class="dropdown-item" type="button">Action</button></li>
-          <li><button class="dropdown-item" type="button">Another action</button></li>
-          <li><button class="dropdown-item" type="button">Something else here</button></li>
-        </ul>
-        </div>
+        <select name="role">
+            <option value="student">Student</option>
+            <option value="teacher">Teacher</option>
+            <option value="admin">Admin</option>
+          </select>
+          <input type="submit" name="submit" value="ADD USER">
 
         <!-- <button type="button" class="btn btn-success">Add Student</button> -->
 
