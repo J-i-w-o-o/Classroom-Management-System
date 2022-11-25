36853f1c17eec288f3264de9d72f331ba18e2a6d
Patrick Renz Garcia <trickrenzgarcia@gmail.com>
2022-11-26 03:13:19 +0800

    hey

--- Components/Database.php
+++ Components/Database.php
@@ -1,3 +1,8 @@
 <?php
   $con = mysqli_connect("localhost", "root", "", "api");
+
+  if(mysqli_connect_errno()){
+    throw new Error("Php mysql connection err: ".mysqli_connect_error());
+  } 
+
 ?>
