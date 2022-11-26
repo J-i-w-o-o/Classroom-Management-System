81644a4a440fc4fa93b118d89f56b1c351a4160a
Jiyoo <42067334+S-o-r-a@users.noreply.github.com>
2022-11-26 19:59:25 +0800

    asdasdasda

--- user/HeadFooter/Header.php
+++ user/HeadFooter/Header.php
@@ -16,7 +16,7 @@ $role       =   $_SESSION['role'];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="../main-css/main.css">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
 </head>
