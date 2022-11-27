a0461aa5a0e7bafb26b5126da7918f8265cade11
dnlsigue <82865898+dnlsigue@users.noreply.github.com>
2022-11-27 23:57:14 +0800

    Edit subject

--- admin/subjectEdit.php
+++ admin/subjectEdit.php
@@ -17,30 +17,29 @@ if (!$id || !$subject || !$subject_code || !$course)
       <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
 
-          <form method="post" class="bg-light border border-primary rounded px-5 ">
+          <form method="post" class="bg-light border border-primary rounded p-5 ">
             <!-- ADD STUDENT MODAL -->
-            <h1 class="text-center pt-3 fw-bold fst-italic">Edit Subject</h1>
-
-            <div class="input-group mb-5 px-1">
-              <span class="input-group-text">Subject</span>
-              <span class="input-group-text" id="basic-addon1"><i class="fa fa-id-badge" aria-hidden="true"></i></span>
-              <input value="<?php echo $subject ?>" type="text" class="form-control" placeholder="Subject" aria-label="Subject" aria-describedby="basic-addon1" name="subject" required>
-            </div>
-
-            <div class="input-group mb-5 px-1">
+            <h1 class="text-center fw-bold mb-5">Edit Subject</h1>
+  
+            <div class="input-group mb-4">
               <span class="input-group-text">Subject Code</span>
               <input value="<?php echo $subject_code ?>" type="text" aria-label="subject_code" placeholder="Subject code" class="form-control" name="subject_code" required>
+            </div>
+
+            <div class="input-group mb-4">
+              <span class="input-group-text">Subject</span>
+              <input value="<?php echo $subject ?>" type="text" class="form-control" placeholder="Subject Name" aria-label="Subject" aria-describedby="basic-addon1" name="subject" required>
+            </div>
 
 
               <!-- SECTION IS DROP DOWN -->
-              <div class="input-group mb-3">
+              <div class="input-group mb-5">
 
                 <div class="input-group-prepend">
                   <label class="input-group-text" for="inputGroupSection">Course</label>
                 </div>
                 <select name="course" id="inputGroupSection" class="custom-select">
                   <?php
-
                   $course = $con->query("SELECT * FROM sections");
                   if ($course->num_rows > 0) {
                     while ($row = $course->fetch_assoc()) { ?>
@@ -56,9 +55,9 @@ if (!$id || !$subject || !$subject_code || !$course)
               </div>
 
 
-              <div class="d-flex justify-content-center py-2 mb-4">
-                <input type="submit" name="submitEdit" class="btn btn-primary btn-lg mx-2" value="Edit"></input>
-                <a href="subjectManage.php"><input type="button" class="btn btn-danger btn-lg mx-2" value="Close"></input></a>
+              <div class="d-flex justify-content-around mt-4">
+                <input type="submit" name="submitEdit" class="btn btn-success btn-lg" value="Save"></input>
+                <a href="subjectManage.php"><input type="button" class="btn btn-danger btn-lg" value="Close"></input></a>
               </div>
 
           </form>
