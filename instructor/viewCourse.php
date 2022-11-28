24c12d03691ffffbc265c292c9e0b53b299fe557
Patrick Renz Garcia <trickrenzgarcia@gmail.com>
2022-11-29 03:49:36 +0800

    uppp

--- instructor/viewCourse.php
+++ instructor/viewCourse.php
@@ -6,7 +6,7 @@
   <div class="container bg-white">
     <?php
       $sql = "
-      SELECT classes.class_code, classes.section, subjects.subject FROM classes INNER JOIN subjects ON classes.class_code=subjects.subject_code WHERE subject_code='".$_GET['subject_code'].
+      SELECT * FROM classes  WHERE subject_code='".$_GET['subject_code'].
       "'";
       $row = $con->query($sql)->fetch_assoc(); 
     ?>
