99d4bfcffb992291231096319351f739cfc2cc84
Patrick Renz Garcia <trickrenzgarcia@gmail.com>
2022-11-29 03:10:37 +0800

    uupppppppp

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
   session_start();
 
-  if(isset($_SESSION['user'])){
+  if(isset($_SESSION['role']) && $_SESSION['role'] == 'admin'){
     header("location: admin/dashboard.php");
-  } else {
+  } else if(isset($_SESSION['role']) && $_SESSION['role'] == 'instructor'){
+    header("location: admin/dashboard.php");
+  } else if(isset($_SESSION['role']) && $_SESSION['role'] == 'student'){
+    header("location: admin/dashboard.php");
+  } 
+  else {
     header("location: login.php");
   }
 ?>
