be107dd0f8bf474261c49c499e69233e366be032
Patrick Renz Garcia <trickrenzgarcia@gmail.com>
2022-11-29 01:53:42 +0800

    my changes

--- instructor/myClass.php
+++ instructor/myClass.php
@@ -1,13 +1,13 @@
 <?php
-    require('includes/Header.php');
-
-    
+  require('includes/Header.php');    
 ?>
 
 <div class="x--main-container">
+
+  <!-- CREATE CLASS MODAL -->
   <?php require './Modals/Add/CreateClassModal.php'; ?>
+
   <div class="container bg-white">
-    <!-- DAPAT NAKA MODAL TONG FORM NATO -->
     
     <div class="container text-end pd-5">
       <button type="button" class="btn btn-success  mb-3 mt-2 fs-6" data-toggle="modal" data-target="#studentModal">
@@ -15,28 +15,29 @@
       </button>
     </div>
 
-    <div>
+    <div class="pb-5">
       <h1>cards</h1>
       <div class="row">
-        <div class="col-sm">
-          <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="..." alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+        <?php 
+          $sql = "SELECT * FROM classes WHERE school_id='". $school_id ."'";
+          $res = $con->query($sql);
+          while($row = $res->fetch_assoc()){
+            ?>
+            <!-- loop happening here... -->
+            <div class="col-lg-4 col-sm-1">
+              <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="https://imageio.forbes.com/specials-images/imageserve/5d7a6f9ec0e1890008d139b9/classroom/960x0.jpg?format=jpg&width=960" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">subject: <?php echo $row['subject_code'] ?></h5>
+                  <h5 class="card-text">section: <?php echo $row['section'] ?></h5>
+                  <a href="#" class="btn btn-primary">View Course</a>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div class="col-sm">
-        One of three columns
-        </div>
-        <div class="col-sm">
-        One of three columns
-        </div>
-        <div class="col-sm">
-        One of three columns
-        </div>
+
+          <?php
+          }
+        ?>
       </div>
       
     </div>
